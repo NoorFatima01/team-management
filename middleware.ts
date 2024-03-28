13f4b57487d7098bd24a7ed863652f42e0bbee3a
Noor Fatima <nfatima101204@gmail.com>
2024-03-28 21:25:53 +0500

    fix: fixed error 'provider not enabled'

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,14 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/my-profile'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.\\.(?:svg|png|jpg|jpeg|gif|webp)$).)',
+  ],
 };
---
+++ src/app/auth/auth-error/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthError = () => {
+  return <div>Authentication Error</div>;
+};
+
+export default AuthError;
