674704d900e10ea7d3cdb6f4ef8431e5004a7819
Amama-Fatima <amamafatima58@gmail.com>
2024-04-07 21:02:16 +0500

    style: gradient

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -2,5 +2,9 @@ import * as React from 'react';
 import '@/lib/env';
 
 export default function HomePage() {
-  return <div>hello</div>;
+  return (
+    <section className='w-full h-screen rounded-md !overflow-visible relative flex flex-col iteams-center antialiased'>
+      <div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(115%_115%_at_50%_10%,#000_40%,#135A13_100%)]'></div>
+    </section>
+  );
 }
