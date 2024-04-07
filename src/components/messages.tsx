98da2d392e3e535b80895d842edeb9042f41e463
Amama-Fatima <amamafatima58@gmail.com>
2024-04-07 10:24:11 +0500

    style: ui improvments to inbox

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -5,6 +5,8 @@ import React, { useEffect } from 'react';
 import { pusherClient } from '@/lib/pusher';
 import useSession from '@/lib/supabase/use-session';
 
+import { ScrollArea } from '@/components/ui/scroll-area';
+
 import { messageUIType } from '@/types';
 
 interface MessagesProps {
@@ -30,35 +32,36 @@ export default function Messages({ teamId, oldChatData }: MessagesProps) {
       pusherClient.unsubscribe(teamId);
     };
   }, [teamId]);
-  //TODO: make the chats box scroll able instead of overflowing
   return (
-    <div className='flex flex-col'>
-      {oldChatData.map((chat, index) => (
-        <div
-          key={index}
-          className={`m-2 flex space-y-0.5 flex-col bg-sky-900 rounded-md p-2  ${
-            chat.sender_id === userId ? 'self-end' : 'self-start'
-          }  max-w-1/2 inline-block`}
-        >
-          <p className='text-muted-foreground text-sm font-semibold'>
-            {chat.sender_name}
-          </p>
-          <p className='text-amber-500'>{chat.message}</p>
-        </div>
-      ))}
-      {incomingChats.map((chat, index) => (
-        <div
-          key={index}
-          className={`m-2 flex space-y-0.5 flex-col bg-sky-900 rounded-md p-2  ${
-            chat.sender_id === userId ? 'self-end' : 'self-start'
-          }  max-w-1/2 inline-block`}
-        >
-          <p className='text-muted-foreground text-sm font-semibold'>
-            {chat.sender_name}
-          </p>
-          <p className='text-amber-500'>{chat.message}</p>
-        </div>
-      ))}
-    </div>
+    <ScrollArea className='h-[25rem]'>
+      <div className='flex flex-col'>
+        {oldChatData.map((chat, index) => (
+          <div
+            key={index}
+            className={`m-4 flex space-y-0.5 flex-col bg-sky-900 rounded-md p-3  ${
+              chat.sender_id === userId ? 'self-end' : 'self-start'
+            }  max-w-1/2 inline-block`}
+          >
+            <p className='text-muted-foreground text-sm font-semibold'>
+              {chat.sender_name}
+            </p>
+            <p className='text-primary-foreground'>{chat.message}</p>
+          </div>
+        ))}
+        {incomingChats.map((chat, index) => (
+          <div
+            key={index}
+            className={`m-4 flex space-y-0.5 flex-col bg-sky-900 rounded-md p-3  ${
+              chat.sender_id === userId ? 'self-end' : 'self-start'
+            }  max-w-1/2 inline-block`}
+          >
+            <p className='text-muted-foreground text-sm font-semibold'>
+              {chat.sender_name}
+            </p>
+            <p className='text-primary-foreground'>{chat.message}</p>
+          </div>
+        ))}
+      </div>
+    </ScrollArea>
   );
 }
