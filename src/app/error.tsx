482cb929c4b10ac1476c39303ab2db2c8a86590d
Noor Fatima <nfatima101204@gmail.com>
2024-04-27 23:54:31 +0500

    chore: fixed small error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
-import TextButton from '@/components/buttons/TextButton';
+import { Button } from '@/components/ui/button';
 
 export default function Error({
   error,
@@ -28,9 +28,9 @@ export default function Error({
           <h1 className='mt-8 text-4xl md:text-6xl'>
             Oops, something went wrong!
           </h1>
-          <TextButton variant='basic' onClick={reset} className='mt-4'>
+          <Button onClick={reset} className='mt-4'>
             Try again
-          </TextButton>
+          </Button>
         </div>
       </section>
     </main>
