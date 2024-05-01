1ae1b252ae43d05a6e7c5a3934488cabba199f03
Amama-Fatima <amamafatima58@gmail.com>
2024-05-01 13:50:00 +0500

    chore: inbox improvements

--- src/app/(lobby)/inbox/page.tsx
+++ src/app/(lobby)/inbox/page.tsx
@@ -1,5 +1,13 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function InboxPage() {
-  return <div>InboxPage</div>;
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='text-3xl font-semibold mt-8 tracking-tighter'>
+        Chat with your team
+      </h1>
+      <Image src='/images/inbox.png' alt='Inbox' width={400} height={400} />
+    </div>
+  );
 }
