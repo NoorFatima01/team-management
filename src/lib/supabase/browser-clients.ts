c4c2bfc34a2e4aca5ea5af6d7e8f9b96a72dc8be
Noor Fatima <nfatima101204@gmail.com>
2024-05-04 17:17:04 +0500

    chore: fixed type error

--- src/lib/supabase/browser-clients.ts
+++ src/lib/supabase/browser-clients.ts
@@ -3,5 +3,5 @@ import { createBrowserClient } from '@supabase/ssr';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@/constant/env';
 
 export function createSupabaseBrowserClient() {
-  return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  return createBrowserClient(SUPABASE_URL as string, SUPABASE_ANON_KEY);
 }
